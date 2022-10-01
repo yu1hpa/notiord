c5fb72bda380500fdb60945c2a129f7149d24e57
yu1hpa <yu.1hpa@gmail.com>
2022-10-02 06:44:23 +0900

    feat: Add Popup component

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -1,9 +1,34 @@
+import { usePopup } from "./hooks/usePopup";
+
 const Popup = () => {
+  const { dbId, apiSec, handleInput, onSubmit, reset } = usePopup();
+
   return (
-    <>
-      <h1>hello</h1>
-      <h1>popup</h1>
-    </>
+    <form onSubmit={onSubmit}>
+      <h2>Notiord</h2>
+      <label>Database ID</label>
+      <input
+        type="password"
+        name="dbId"
+        onChange={handleInput}
+        value={dbId}
+        required
+      />
+      <label>API Secret</label>
+      <input
+        type="password"
+        value={apiSec}
+        name="apiSec"
+        onChange={handleInput}
+        required
+      />
+      <h4>input is hidden for security</h4>
+      <p>Cmd/Ctrl + Shift + L : open setting popup</p>
+      <p>Cmd/Ctrl + Shift + I : open new page</p>
+      <button type="submit">Save</button>
+      <button onClick={reset}>Reset</button>
+      {/* check dbid & apisec are valid */}
+    </form>
   );
 };
 
